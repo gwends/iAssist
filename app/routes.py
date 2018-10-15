9d39e221bacb3b73baf55f506230a08658b6f47c
unknown <reiljhoseph@yahoo.com>
2018-10-16 03:00:53 +0800

    Initial Search page to be edited

--- app/routes.py
+++ app/routes.py
@@ -13,4 +13,9 @@ def index():
 def job_apply():
     employeeForm = EmployeeJobPostForm()
     hirerForm = HirerJobPostForm()
-    return render_template('job_apply.html', title="Job Apply", eform=employeeForm, hform=hirerForm)
+    return render_template('job_apply.html', title="Job Post", eform=employeeForm, hform=hirerForm)
+
+
+@app.route('/job_search')
+def job_search():
+    return render_template('job_search.html', title='Job Search')
